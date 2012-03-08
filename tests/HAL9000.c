0f9a6dcebb9a217c539062be39c722108ad6454c
= <martinnjorgensen@gmail.com>
2012-03-08 21:46:21 +0100

    Started on an awesomeness shell of win and doom.

---
+++ tests/HAL9000.c
@@ -0,0 +1,16 @@
+#include "tests/lib.h"
+
+void execute(char *in) {
+    
+}
+
+int main() {
+    puts("Hello Dave.\n");
+    char *in;
+    int len = 0;
+    len = readline(in,255);
+
+    execute(in);
+
+    return 0;
+}
